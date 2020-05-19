0509354c2a43a627898c623a683007d60d37f5ed
David Wightman <david@davidwightman.org>
2020-05-19 08:03:09 -0400

    added book controller. set up h2. more on mvc

---
+++ src/main/java/org/davidwightman/webapplication/webapplication/controllers/BookController.java
@@ -0,0 +1,23 @@
+package org.davidwightman.webapplication.webapplication.controllers;
+
+import org.davidwightman.webapplication.webapplication.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
